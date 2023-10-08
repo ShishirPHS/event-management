ebae40fef716e1e3ee6df989ca122145eff0702c
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-09 02:08:38 +0600

    designed service details card of dynamic route

--- src/pages/ServiceDetails/ServiceDetails.jsx
+++ src/pages/ServiceDetails/ServiceDetails.jsx
@@ -6,11 +6,18 @@ const ServiceDetails = () => {
 
   const service = allServices.find((singleService) => singleService.id == id);
 
-  const { title } = service;
+  const { title, image, price, short_description } = service;
 
   return (
-    <div className="container mx-auto">
-      <h2>service details of :{title}</h2>
+    <div className="container mx-auto flex justify-center mb-28">
+      <div className="flex flex-col items-center w-1/2 border p-20 rounded-2xl">
+        <img className="rounded-lg" src={image} alt={`image for ${title}`} />
+        <div className="mt-5 mb-8 flex justify-between w-full font-semibold">
+          <h3 className="text-3xl">{title}</h3>
+          <p className="text-2xl">{price}</p>
+        </div>
+        <p className="text-justify">{short_description}</p>
+      </div>
     </div>
   );
 };

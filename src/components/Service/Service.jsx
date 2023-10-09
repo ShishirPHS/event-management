47a86a7833b30b8b529d6e890d336f8c66dd2ff6
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-09 21:51:37 +0600

    home page responsive for desktop devices

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -6,23 +6,23 @@ const Service = ({ singleData }) => {
     singleData;
 
   return (
-    <div className="rounded-lg border shadow-inner p-5">
+    <div className="rounded-lg border shadow-inner p-5 flex flex-col">
       <img
-        className="rounded-lg h-[250px] object-cover w-full"
+        className="rounded-lg lg:h-[180px] xl:h-[250px] object-cover w-full"
         src={image}
         alt={`Image for ${title}`}
       />
-      <div className="mt-3">
+      <div className="mt-3 flex-grow">
         <h3 className="mb-4 text-2xl text-[#1C1B1B] font-medium">{title}</h3>
         <p>{short_description.slice(0, 150)}...</p>
-        <p className="font-bold my-3">Price: {price}</p>
-        <div className="text-center">
-          <Link to={`/service/${id}`}>
-            <button className="w-full py-3 rounded-lg text-white bg-[#4a4040]">
-              {details_button}
-            </button>
-          </Link>
-        </div>
+      </div>
+      <p className="font-bold my-3">Price: {price}</p>
+      <div className="text-center">
+        <Link to={`/service/${id}`}>
+          <button className="w-full py-3 rounded-lg text-white bg-[#4a4040]">
+            {details_button}
+          </button>
+        </Link>
       </div>
     </div>
   );
